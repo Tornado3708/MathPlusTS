34b8980f0e9501b24049096c8f6979ff788605eb
Tornado3708 <sami.vagebi@gmail.com>
2023-01-20 23:11:54 +0200

    index file uploading

---
+++ mathplus.ts
@@ -0,0 +1,76 @@
+import * as methods      from "./install.js"
+
+import Complex           from "./math/comlpex/Complex.js"
+
+import Matrix            from "./math/algebra/linear_algebra/matrix/Matrix.js"
+
+import VectorCylindrical from "./math/algebra/linear_algebra/vector/VectorCylindrical.js"
+import Vector2D          from "./math/algebra/linear_algebra/vector/Vector2D.js"
+import Vector3D          from "./math/algebra/linear_algebra/vector/Vector3D.js"
+import VectorPolar       from "./math/algebra/linear_algebra/vector/VectorPolar.js"
+import VectorSpherical   from "./math/algebra/linear_algebra/vector/VectorSpherical.js"
+
+
+
+import Polygon           from "./math/geometry/curves/Polygon.js"
+import Roulette          from "./math/geometry/curves/Roulette.js"
+import Spiral            from "./math/geometry/curves/Spiral.js"
+import Tensor from "./math/algebra/linear_algebra/tensor/Tensor.js"
+
+
+
+
+declare global {
+  
+  interface Math {
+
+  Complex           :typeof Complex
+  
+  Tensor            :typeof Tensor
+
+  Matrix            :typeof Matrix
+  
+  Vector2D          :typeof Vector2D
+  Vector3D          :typeof Vector3D
+  VectorPolar       :typeof VectorPolar
+  VectorSpherical   :typeof VectorSpherical
+  VectorCylindrical :typeof VectorCylindrical
+
+  Polygon:  Polygon
+  Roulette: Roulette
+  Spiral:   Spiral
+
+  factorial: (n :number) => number
+
+  sinc:     (x: number ,norm: number) => number
+  cosc:     (x: number ,norm: number) => number
+
+  cas:      (x: number) => number
+
+  sec:      (x: number) => number
+  cosec:    (x: number) => number
+
+
+  toDegrees:(radians: number) => number
+  toRadians:(degrees: number) => number
+
+
+
+  PI_QUARTER: number
+  PI_HALF:    number
+  PI_TWO:     number
+  TAU:        number
+
+  SQRT3:      number
+  SQRT5:      number
+
+  PHI:        number
+  phi:        number
+
+  RAD2DEG:    number
+  DEG2RAD:    number
+  }
+}
+
+
+export default methods.install
