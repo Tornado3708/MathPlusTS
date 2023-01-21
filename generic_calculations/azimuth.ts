ad873f9056cbad0e21fc4d42546ee06d3a3aa71d
Tornado3708 <sami.vagebi@gmail.com>
2023-01-21 20:51:42 +0200

    refactoring and jsdoc

--- generic_calculations/azimuth.ts
+++ generic_calculations/azimuth.ts
@@ -1,1 +1,7 @@
-export default function azimuth(y=0 , x=0): number { return Math.atan2(y,x) }
+/**
+ * Refactoring function. Returns Math.atan2(y,x)
+ * @param {number} [y] [y] 
+ * @param {number} [x] [x]
+ * @returns Angle in radians.
+ * */
+export default function azimuth(y:number=0 , x:number=0) :number { return Math.atan2(y,x) }
--- generic_calculations/magnitude.ts
+++ generic_calculations/magnitude.ts
@@ -1,1 +1,6 @@
+/**
+ * Refactoring function. Returns Math.hypot(values).
+ * @param {number[]} values [values]
+ * @returns Number
+ */
 export default function magnitude(...values: number[]): number { return Math.hypot(...values) }
