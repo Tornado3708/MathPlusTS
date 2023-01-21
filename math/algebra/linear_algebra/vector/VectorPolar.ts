bfd4a81ea6ea8d237cdf591994337ba9f086d26f
Tornado3708 <sami.vagebi@gmail.com>
2023-01-21 22:44:38 +0200

    refactoring and jsdoc

--- math/algebra/linear_algebra/vector/VectorPolar.ts
+++ math/algebra/linear_algebra/vector/VectorPolar.ts
@@ -1,16 +1,22 @@
 import Vector2D from "./Vector2D.js"
+import { simple2D } from "../../../../simplify.js"
 
 
 function toArray(obj :PointPolar){ return [ obj.azimuth , obj.radius ] }
 
 
+/**Class for representation of 2-dimensional radius vector.*/
 class VectorPolar implements PointPolar {
 
 
   public azimuth :number = 0
   public radius  :number = 0
 
-
+  /**
+   * - { azimuth : number , radius : number }
+   * - [ azimuth : number , radius : number ]
+   * @param {PointPolar | number[]} params 
+   * */
   constructor({...params}){
     
     this.azimuth = params.azimuth ?? params[0] ?? 0
@@ -23,35 +29,110 @@ class VectorPolar implements PointPolar {
   }
 
 
+
+
+  /**
+   * Rotates polar vector by azimuth.
+   * @param {number} [azimuth] Rotation, relative to current azimuth.
+   * */
   rotate(azimuth :number=0) :void { VectorPolar.rotate(this, azimuth) }
 
-  radiusAdd (add :number=0) :void { VectorPolar.radiusAdd(this, add) }
-  radiusSub (sub :number=0) :void { VectorPolar.radiusAdd(this, -sub) }
-  radiusMult(m :number=1)   :void { VectorPolar.radiusMult(this, m) }
-  radiusDiv (d :number=1)   :void { VectorPolar.radiusMult(this, 1 / d) }
 
-  get cartesian()       :Point2D  { return VectorPolar.cartesian(this) }
-  get cartesianVector() :Vector2D { return VectorPolar.cartesianVector(this) }
+
+
+  /**
+   * Extends radius by [add].
+   * @param {number} [add] [add]
+   * */
+  radiusAdd(add :number=0) :void { VectorPolar.radiusAdd(this , add) }
+
+  /**
+   * Decrease radius by [sub].
+   * @param {number} [sub] [sub]
+   * */
+  radiusSub(sub :number=0) :void { VectorPolar.radiusAdd(this , -sub) }
+
+  /**
+   * Multiplicates radius length by [m].
+   * @param {number} [m] [m]
+   * */
+  radiusMult(m :number=1) :void { VectorPolar.radiusMult(this , m) }
   
+  /**
+   * Divides radius length by [d].
+   * @param {number} [d] [d]
+   * */
+  radiusDiv(d :number=1) :void { VectorPolar.radiusMult(this , 1 / d) }
 
+
+
+
+  /**Returns parameters of this polar vector in cartesian coordinate system.*/
+  get cartesian() :Point2D { return VectorPolar.cartesian(this) }
+
+  /**Returns 3-dimensional cartesian vector, equivalent to this vector.*/
+  get cartesianVector() :Vector2D { return VectorPolar.cartesianVector(this) }
+
+
+
+
+  /**
+   * Rotates given vector by [azimuth], relative to current polar vector azimuth.
+   * @param {PointPolar} [polar] Given vector.  
+   * @param {number} azimuth Rotation, relative to current azimuth.
+   * */
   static rotate(polar :PointPolar, azimuth :number=0) :void {
     if(isFinite(azimuth))polar.azimuth += azimuth
     if(Math.abs(polar.azimuth) > Math.TAU) polar.azimuth %= Math.TAU
   }
 
-  static radiusAdd (polar :PointPolar, add :number=0) :void { polar.radius += add }
+
+
+
+  /**
+   * Extends radius by [add] for given vector.
+   * @param {PointPolar} [polar] Given vector.
+   * @param {number} [add] [add]
+   * */
+  static radiusAdd(polar :PointPolar, add :number=0) :void { polar.radius += add }
+
+  /**
+   * Decrease radius by [sub] for given vector.
+   * @param {PointPolar} [polar] Given vector.
+   * @param {number} [sub] [sub]
+   * */
+  static radiusSub(polar :PointPolar, sub: number=0) :void { polar.radius -= sub }
+
+  /**
+   * Multiplicates radius length by [m] times for given vector.
+   * @param {PointPolar} [polar] Given vector.
+   * @param {number} [m] [m]
+   * */
   static radiusMult(polar :PointPolar, m :number=1)   :void { polar.radius *= m } 
-  static radiusSub (polar :PointPolar, sub: number=0) :void { polar.radius -= sub }
+  
+  /**
+   * Divides radius length by [d] times for given vector.
+   * @param {PointPolar} polar Given vector.
+   * @param {number} [d] [d] 
+   * */
   static radiusDiv (polar :PointPolar, d :number=1)   :void { this.radiusMult(polar, 1 / d) }
   
-  static cartesian({azimuth=0,radius=0} :PointPolar) :Point2D { return {
-      x: Math.cos(azimuth) * radius,
-      y: Math.sin(azimuth) * radius
-    }
-  }
 
+
+
+  /**
+   * Returns parameters of given vector in 2-dimensional cartesean coordinates system.
+   * @param {PointPolar} param0 Given vector.
+   * @returns Point2D
+   * */
+  static cartesian({azimuth=0,radius=0} :PointPolar) :Point2D { return simple2D(Math.cos(azimuth) * radius , Math.sin(azimuth) * radius) }
+
+  /**
+   * Returns 2-dimensional cartesean vector, equivalent to given polar vector.
+   * @param {PointPolar} polar Given vector.
+   * @returns Vector2D
+   * */
   static cartesianVector(polar :PointPolar) :Vector2D { return new Vector2D(this.cartesian(polar))}
-
 
 
 
