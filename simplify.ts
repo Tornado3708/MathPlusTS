2f117ae47bab9a8087ff478a89dc2009709e1ff4
Tornado3708 <sami.vagebi@gmail.com>
2023-01-22 18:35:57 +0200

    refactoring

--- simplify.ts
+++ simplify.ts
@@ -12,4 +12,4 @@ export const simplier3D = ({x=0,y=0,z=0}) :point3D => { return { x , y , z } }
 
 export const simplierPolar       = ({azimuth=0,radius=0})          :polar       => { return { azimuth , radius } }
 export const simplierSpherical   = ({azimuth=0,zenith=0,radius=0}) :spherical   => { return { azimuth , zenith , radius } }
-export const simplierCylindrical = ({azimuth=0,radius=0,z=0})      :cylindrical =>  {return { azimuth , radius , z } }
+export const simplierCylindrical = ({azimuth=0,radius=0,z=0})      :cylindrical => { return { azimuth , radius , z } }
