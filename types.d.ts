2db25e5ff81c9a878ee22a0fde3212653bd3166b
Tornado3708 <sami.vagebi@gmail.com>
2023-01-22 13:25:22 +0200

    types and interfaces renaming

--- types.d.ts
+++ types.d.ts
@@ -1,13 +1,13 @@
-interface Point2D { x: number, y: number }
-interface Point3D extends Point2D { z: number }
+interface point2D { x: number, y: number }
+interface point3D extends point2D { z: number }
 
-interface PointPolar { azimuth: number, radius: number }
-interface PointSpherical extends PointPolar { zenith: number }
-interface PointCylindrical extends PointPolar { z: number }
+interface polar { azimuth: number, radius: number }
+interface spherical extends polar { zenith: number }
+interface cylindrical extends polar { z: number }
 
-interface Round2D extends Point2D { radius: number }
-interface Round3D extends Round2D { z: number }
+interface round2D extends point2D { radius: number }
+interface round3D extends round2D { z: number }
 
-interface poly_regular extends Point2D , PointPolar{ gons :number }
+interface poly_regular extends point2D , polar { gons :number }
 
 type matrix = number[][]
