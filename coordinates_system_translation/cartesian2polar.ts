a87438c865244ec53b11b4a72c6dd5902ebb4e9f
Tornado3708 <sami.vagebi@gmail.com>
2023-01-22 18:32:52 +0200

    jsdoc

--- coordinates_system_translation/cartesian2polar.ts
+++ coordinates_system_translation/cartesian2polar.ts
@@ -1,8 +1,13 @@
-import azimuth from "../generic_calculations/azimuth.js";
-import magnitude from "../generic_calculations/magnitude.js";
-import { simplePolar } from "../simplify.js";
+import azimuth         from "../generic_calculations/azimuth.js"
+import magnitude       from "../generic_calculations/magnitude.js"
+import { simplePolar } from "../simplify.js"
 
-export default function cart2polar({x=0,y=0} :point2D){
+/**
+ * Returns cartesian position to polar position.
+ * @param {point2D} [param0] Object.{x: number, y: number} 
+ * @returns polar
+ * */
+export default function cart2polar({x=0,y=0} :point2D): polar {
   let mag  = magnitude(x,y)
   let azim = azimuth  (y,x)
   return simplePolar(azim,mag)
