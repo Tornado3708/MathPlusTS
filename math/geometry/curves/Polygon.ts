09df141fc0e4137e5727e3de1391dcac3591c7d6
Tornado3708 <sami.vagebi@gmail.com>
2023-02-05 13:43:46 +0200

    Added Star class with static methods and perimeter

--- math/geometry/curves/Polygon.ts
+++ math/geometry/curves/Polygon.ts
@@ -1,9 +1,28 @@
-import PolygonRegular from "./polygons/PolygonRegular.js";
+import magnitude from "../../../generic_calculations/magnitude.js"
+import PolygonRegular from "./polygons/PolygonRegular.js"
+import Star from "./polygons/Star.js"
 
-class Polygon {
+/**
+ * Representations of polygons.
+ * 
+ * - [Regular] - regular polygon.
+ * */
+export default class Polygon {
 
-  static PolygonRegular = PolygonRegular;
+  static Regular = PolygonRegular
+  static Star    = Star
 
+  static sides(polygon :matrix){
+    let sides = []
+    for(let i = 0; i < polygon.length; i++){
+      sides.push(Math.hypot(
+        polygon[i][0] - (i < polygon.length ? polygon[i][0] : polygon[0][0]) , polygon[i][1] - polygon[(i + 1) || 0][1]))
+    }
+    return sides
+  }
+  static perimeter(polygon :matrix){
+    let perimeter = 0
+    this.sides(polygon).forEach( side => perimeter += side )
+    return perimeter
+  }
 }
-
-export default Polygon;
